fb43686f2d1ca4ee14e8da8d146e6835093be342
Jesse Reichman <14865360+jreichman@users.noreply.github.com>
2024-06-17 13:28:07 +0930

    Update vmware to use AutomationUtils

--- src/library/vmware.ps1
+++ src/library/vmware.ps1
@@ -4,6 +4,11 @@
 [CmdletBinding()]
 param()
 
+# Global settings
+Set-StrictMode -Version 2
+$ErrorActionPreference = "Stop"
+$InformationPreference = "Continue"
+
 # Modules
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 @("VMware.VimAutomation.Core") | ForEach-Object {
@@ -14,12 +19,12 @@ Set-PSRepository PSGallery -InstallationPolicy Trusted
 
 # Functions
 
-Function Remove-VMSnapshots
-{
+Register-Automation -Name vmware.snapshot_cleanup -ScriptBlock {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory=$true,ValueFromPipeline)]
-        $vm,
+        [Parameter(Mandatory=$true)]
+        [AllowEmptyCollection()]
+        $vms,
 
         [Parameter(Mandatory=$true)]
         [ValidateNotNull()]
@@ -29,43 +34,75 @@ Function Remove-VMSnapshots
     process
     {
         # Retrieve snapshots older than Age
-        $snapshots = $vm | Get-Snapshot | Where-Object { $_.Created -lt ([DateTime]::Now.AddDays(-($Age))) }
+        $snapshots = $vms | Get-Snapshot | Where-Object { $_.Created -lt ([DateTime]::Now.AddDays(-($Age))) }
 
         # Finish here if no snapshots found
         if (($snapshots | Measure-Object).Count -lt 1)
         {
+            Write-Information "No snapshots meeting age threshold found"
             return
         }
 
-        # Remove each snapshot and return a copy of the snapshot object
+        # Remove each snapshot
         $snapshots | ForEach-Object {
-            $_
-            $_ | Remove-Snapshot -Confirm:$false
+            Write-Information ("Removing snapshot: " + $snapshot.Name)
+            $_ | Remove-Snapshot -Confirm:$false | Out-Null
         }
+
+        # Log a notification for the removed snapshots
+        $capture = New-Capture
+        & {
+            Write-Information "Removed snapshots:"
+            Write-Information ($snapshots | Format-Table VM,Name,Created,SizeGB | Out-String)
+        } *>&1 | Copy-ToCapture -Capture $capture
+
+        New-Notification -Title "Snapshots removed" -Body ($capture | Out-String)
     }
 }
 
-Function Invoke-VMConsolidate
-{
+Register-Automation -Name vmware.vm_consolidate -ScriptBlock {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory=$true,ValueFromPipeline)]
-        $vm
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [AllowEmptyCollection()]
+        $vms
     )
 
     process
     {
-        # Do nothing if this VM doesn't require consolidation
-        if (!$vm.ExtensionData.Runtime.ConsolidationNeeded)
+        # Consolidate VMs requiring it and record VMs changed
+        $consolidated = $vms | ForEach-Object {
+            $vm = $_
+
+            # Do nothing if this VM doesn't require consolidation
+            if (!$vm.ExtensionData.Runtime.ConsolidationNeeded)
+            {
+                return
+            }
+
+            # Consolidate VM
+            Write-Information ("Consolidating VM: " + $vm.Name)
+            $vm.ExtensionData.ConsolidateVMDisks()
+
+            $vm
+        }
+
+        # Finish here if no VMs required consolidation
+        if (($consolidated | Measure-Object).Count -lt 1)
         {
+            Write-Information "No VMs required consolidation"
             return
         }
 
-        # Consolidate VM
-        $vm.ExtensionData.ConsolidateVMDisks()
+        # Log a notification as we consolidated some VMs
+        $capture = New-Capture
+        & {
+            Write-Information "VMs consolidated:"
+            Write-Information ($consolidated | ForEach-Object { $_.Name })
+        } *>&1 | Copy-ToCapture -Capture $capture
 
-        $vm
+        New-Notification -Title "VMs Consolidated" -Body ($capture | Out-String)
     }
 }
-
 
